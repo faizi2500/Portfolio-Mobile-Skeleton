d4695dad0ead6919e9434fcfdb7a4cb704a285ba
Juliana Ifionu <julieifionu@gmail.com>
2021-07-26 14:23:29 +0100

    Add event listener for mobile menu section links

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const mobileMenuSection = document.querySelector('.mobile-menu-section')
-const crossButton = document.querySelector('.cross-button')
-const hamburger = document.querySelector('.hamburger')
+const mobileMenuSection = document.querySelector('.mobile-menu-section');
+const crossButton = document.querySelector('.cross-button');
+const hamburger = document.querySelector('.hamburger');
+const mobileLinks = document.querySelectorAll('.mobile-links-color');
 
-let menu = false
+const list = Array.from(mobileLinks);
+
+let menu = false;
 
 function closeMenu() {
   mobileMenuSection.style.display = 'none';
@@ -14,8 +17,12 @@ function openMenu() {
     mobileMenuSection.style.display = 'block';
     menu = true;
   } else closeMenu();
-
 }
 
-hamburger.addEventListener('click', openMenu)
-crossButton.addEventListener('click', closeMenu)
+hamburger.addEventListener('click', openMenu);
+crossButton.addEventListener('click', closeMenu);
+
+list.forEach((list) => {
+  list.addEventListener('click', closeMenu);
+})
+

bc376a8fc912db531d94bba9aed6c0faa6b80ba7
Juliana Ifionu <julieifionu@gmail.com>
2021-07-26 14:14:05 +0100

    Add event to close button

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ const hamburger = document.querySelector('.hamburger')
 
 let menu = false
 
+function closeMenu() {
+  mobileMenuSection.style.display = 'none';
+  menu = false;
+}
+
 function openMenu() {
   if (menu == false) {
     mobileMenuSection.style.display = 'block';
     menu = true;
-  }
+  } else closeMenu();
 
 }
 
 hamburger.addEventListener('click', openMenu)
+crossButton.addEventListener('click', closeMenu)

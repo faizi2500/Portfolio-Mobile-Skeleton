16c3f08c4fe0591fac5e3a452e885e110bab2a4e
Faizan <faizan.zahid2500@gmail.com>
2021-07-27 15:52:24 +0500

    Add JS file containing objects and arrays

---
+++ data.js
@@ -0,0 +1,99 @@
+let projects = {
+  firstProject : {
+    image: {
+      imageUrl: './assets/images/work-project1.png', 
+      altText: 'This image is a project thumbnail',
+    },
+    projectHeading: 'Tonic Project: Personalized Reading',
+    popupHeading: 'Keeping track of hundreds of components',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    languages: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    projectButton: 'See Project',
+    liveVersion: 'https://faizi2500.github.io/Portfolio-Mobile-Skeleton/',
+    sourceCode: 'https://github.com/faizi2500'
+  },
+
+  secondProject : {
+    image: {
+      imageUrl: './assets/images/work-project2.png', 
+      altText: 'This image is a project thumbnail',
+    },
+    projectHeading: 'Multi Post Stories: Gain + Glory',
+    popupHeading: 'Keeping track of hundreds of components',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    languages: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    projectButton: 'See Project',
+    liveVersion: 'https://faizi2500.github.io/Portfolio-Mobile-Skeleton/',
+    sourceCode: 'https://github.com/faizi2500'
+  },
+
+  secondProject : {
+    image: {
+      imageUrl: './assets/images/work-project2.png', 
+      altText: 'This image is a project thumbnail',
+    },
+    projectHeading: 'Multi Post Stories: Gain + Glory',
+    popupHeading: 'Keeping track of hundreds of components',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    languages: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    projectButton: 'See Project',
+    liveVersion: 'https://faizi2500.github.io/Portfolio-Mobile-Skeleton/',
+    sourceCode: 'https://github.com/faizi2500'
+  },
+
+  thirdProject : {
+    image: {
+      imageUrl: './assets/images/work-project3.png', 
+      altText: 'This image is a project thumbnail',
+    },
+    projectHeading: 'Health and Fitness Project',
+    popupHeading: 'Keeping track of hundreds of components',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    languages: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    projectButton: 'See Project',
+    liveVersion: 'https://faizi2500.github.io/Portfolio-Mobile-Skeleton/',
+    sourceCode: 'https://github.com/faizi2500'
+  },
+
+  forthProject : {
+    image: {
+      imageUrl: './assets/images/work-project4.png', 
+      altText: 'This image is a project thumbnail',
+    },
+    projectHeading: 'Art Printing Portfolio',
+    popupHeading: 'Keeping track of hundreds of components',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    languages: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    projectButton: 'See Project',
+    liveVersion: 'https://faizi2500.github.io/Portfolio-Mobile-Skeleton/',
+    sourceCode: 'https://github.com/faizi2500'
+  },
+
+  fifthProject : {
+    image: {
+      imageUrl: './assets/images/work-project4.png', 
+      altText: 'This image is a project thumbnail',
+    },
+    projectHeading: 'Art Printing Portfolio',
+    popupHeading: 'Keeping track of hundreds of components',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    languages: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    projectButton: 'See Project',
+    liveVersion: 'https://faizi2500.github.io/Portfolio-Mobile-Skeleton/',
+    sourceCode: 'https://github.com/faizi2500'
+  },
+
+  sixthProject : {
+    image: {
+      imageUrl: './assets/images/work-project4.png', 
+      altText: 'This image is a project thumbnail',
+    },
+    projectHeading: 'Art Printing Portfolio',
+    popupHeading: 'Keeping track of hundreds of components',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    languages: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    projectButton: 'See Project',
+    liveVersion: 'https://faizi2500.github.io/Portfolio-Mobile-Skeleton/',
+    sourceCode: 'https://github.com/faizi2500'
+  }, 
+}

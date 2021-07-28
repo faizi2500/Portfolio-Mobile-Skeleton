4e686550c0af9476708468346b16cda1b5c404bc
Juliana Ifionu <julieifionu@gmail.com>
2021-07-28 21:54:55 +0100

    Add validation function to validation.js

--- validation.js
+++ validation.js
@@ -1,12 +1,19 @@
 const formSection = document.querySelector('.desktop-form');
-const emailControl =document.querySelector('#email-id');
+const emailControl = document.querySelector('#email-id');
 
 const alertMessage = document.createElement('p');
 alertMessage.classList.add('error-Message');
-alertMessage.textContent = "Your Email Address contains UPPERCASE letters, please lowercase letters. (e.g username@gmail.com"; 
-const errorParentDiv = document.querySelector('error-parent');
+alertMessage.textContent ='Your Email Address contains UPPERCASE letters, please lowercase letters. (e.g username@gmail.com';
+const errorParentDiv = document.querySelector('.error-parent');
 errorParentDiv.appendChild(alertMessage);
 
-formSection.addEventListener('submit', validationFunct)
+function validationFunct(e) {
+  if (emailControl.value !== emailControl.value.toLowerCase()) {
+    errorParentDiv.style.display = 'block';
+    e.preventDefault();
+  } else {
+    errorParentDiv.style.display = 'none';
+  }
+}
 
-
+formSection.addEventListener('submit', validationFunct);

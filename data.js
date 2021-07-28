afdc8f202fda250f931743cadd0ad0b648e6b1f9
Juliana Ifionu <julieifionu@gmail.com>
2021-07-28 14:19:03 +0100

    Add modal to project card using JavaScript

--- data.js
+++ data.js
@@ -1,27 +1,11 @@
-let projects = {
+let projectList = {
   firstProject: {
     image: {
       imageUrl: './assets/images/work-project1.png',
       altText: 'This image is a project thumbnail',
-      modalImage: './assets/images/modal-img.png',
+      modalImage: './assets/images/modal-img',
     },
     projectHeading: 'Tonic Project: Personalized Reading',
-    popupHeading: 'Keeping track of hundreds of components',
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    languages: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-    projectButton: 'See Project',
-    liveVersion: 'https://faizi2500.github.io/Portfolio-Mobile-Skeleton/',
-    sourceCode: 'https://github.com/faizi2500',
-  },
-
-  secondProject: {
-    image: {
-      imageUrl: './assets/images/work-project2.png',
-      altText: 'This image is a project thumbnail',
-      modalImage: './assets/images/modal-img.png',
-    },
-    projectHeading: 'Multi Post Stories: Gain + Glory',
     popupHeading: 'Keeping track of hundreds of components',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
@@ -51,7 +35,7 @@ let projects = {
     image: {
       imageUrl: './assets/images/work-project3.png',
       altText: 'This image is a project thumbnail',
-      modalImage: './assets/images/modal-img.png',
+      modalImage: './assets/images/modal-img',
     },
     projectHeading: 'Health and Fitness Project',
     popupHeading: 'Keeping track of hundreds of components',
@@ -112,96 +96,52 @@ let projects = {
   },
 };
 
-const gridDiv = document.querySelector('.container-grid');
+const projects = [
+  projectList.firstProject,
+  projectList.secondProject,
+  projectList.thirdProject,
+  projectList.forthProject,
+  projectList.fifthProject,
+  projectList.sixthProject,
+];
 
-function gridFunction(project, index) {
-  let cardText = `<div class="section-container1 section-box item-grid">
-  <ul class="first-work display-flex-1">
-    <li class="project1-image display-flex-width">
-      <img
-        class="p1"
-        src="${project.image.imageUrl}"
-        alt=${project.image.altText}
-        rel="noopener noreferrer"
-      />
-    </li>
-    <li class="display-flex-width heading-height">
-      <h3 class="project-heading">
-        <a
-          href="#/"
-          class="project-heading-color"
-          rel="noopener noreferrer"
-        >
-          ${project.projectHeading}
-        </a>
-      </h3>
-    </li>
-    <li class="display-flex-width languages-used">
-      <ul class="languages language-flex">`;
-  for (let i = 0; i < project.languages.length; i += 1) {
-    cardText += `<li>${project.languages[i]}</li>`;
+function modalFunction(number) {
+  const modalDisplay = document.querySelector('#modal');
+  modalDisplay.classList.toggle('invisible');
+  const modaltitle = document.querySelector('.modal-heading');
+  const modallist = document.querySelector('.modal-list');
+  modallist.innerHTML = '';
+  const modalImg = document.querySelector('.img');
+  const modalDesc = document.querySelector('.modal-p');
+  const buttonLive = document.querySelector('.f-icon-1');
+
+  modaltitle.innerHTML = projects[number].popupHeading;
+
+  for (let i = 0; i < projects[number].languages.length; i += 1) {
+    const list = document.createElement('li');
+    list.textContent = projects[number].languages[i];
+    modallist.appendChild(list);
   }
+  buttonLive.setAttribute(
+    'onclick',
+    `windows.open('${projects[number].liveVersion}', '_blank');`
+  );
+  buttonLive.setAttribute(
+    'onclick',
+    `windows.open('${projects[number].sourceCode}', '_blank');`
+  );
+  modalDesc.innerHTML = projects[number].description;
 
-  cardText += `</ul>
-    </li>
-      <li class="display-flex-width btn-3 btn-project-1">
-      <button class="action-button" type="button">See Project</button>
-      </li>
-  </ul>
-</div>`;
-
-  return cardText
+  modalImg.setAttribute(
+    'src',
+    `${projects[number].image.modalImage + (number + 1)}.png`
+  );
 }
 
-
-function modalFunction(project, index) {
-  let cardText = 
-  `<section class="modal-section" id="modal">
-    <div class="modal-container">
-      <div class="modal-content">
-        <div class="times">&times;</div>
-        <div class="modal-image">
-          <img src="${project.image.modalImage}" alt="${project.image.altText}"/>
-        </div>
-        <div class="modal-title">
-          <h2 class="modal-title about-heading">${project.popupHeading}</h2>
-        </div>
-        <ul class="modal-list">`;
-    for (let i = 0; i < project.languages.length; i += 1) {
-        cardText += `<li>${project.languages[i]}</li>`;
-    }
-
-      cardText += 
-        `</ul>
-        <div class="modal-paragraph">
-          <p>${project.description}</p>
-        </div>
-        <div class="btn">
-          <a href="https://faizi2500.github.io/Portfolio-Mobile-Skeleton/">
-          ${project.liveVersion} <i class="fas fa-external-link-alt f-icon"></i>
-          </a>
-          <a href="https://github.com/faizi2500/Portfolio-Mobile-Skeleton">
-          ${project.sourceCode} <i class="fab fa-github f-icon"></i>
-          </a>
-        </div>
-      </div>
-    </div>`;
-
-    return cardText
+function toggle() {
+  const card = document.querySelector('#modal');
+  card.classList.toggle('invisible');
 }
 
-function addHTML() {
-  const projNames = Object.keys(projects);
-  const projLength = Object.keys(projects).length;
-  for (let i = 0; i< projLength; i++) {
-    gridDiv.innerHTML += gridFunction(projects[projNames[i]], i);
-  }
-  
-  for (let j=0; j <projLength; i++) {
-    gridDiv.innerHTML += modalFunction(projects[projNames[i]], i);
-  }
-}
-
-
-
-gridDiv.onload = addHTML();
+modalFunction(1);
+toggle();

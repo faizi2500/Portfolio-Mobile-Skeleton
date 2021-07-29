00be8fcb9ab155295712cf98161110fd6882618b
Juliana Ifionu <julieifionu@gmail.com>
2021-07-29 16:16:34 +0100

    Add function for preserving data locally

--- localstorage.js
+++ localstorage.js
@@ -10,5 +10,14 @@ function populateForm() {
   localStorage.setItem('name', document.getElementById('name').value);
   localStorage.setItem('email-id', document.getElementById('email-id').value);
   localStorage.setItem('text-area', document.getElementById('text-area').value);
-  setStorage();
+  setLocal();
+}
+
+function setLocal() {
+  const localName = localStorage.getItem('name');
+  const localemail = localStorage.getItem('email-id');
+  const localText = localStorage.getItem('text-area');
+  document.getElementById('name').value = localName;
+  document.getElementById('email-id').value = localemail;
+  document.getElementById('text-area').value = localText;
 }

d7534ba390a32ca24320e2e48dd53e2359d22b0f
Faizan <faizan.zahid2500@gmail.com>
2021-07-29 20:28:35 +0500

    JS issues resolved

--- localstorage.js
+++ localstorage.js
@@ -2,17 +2,6 @@ const userFullName = document.getElementById('name');
 const userEmailId = document.getElementById('email-id');
 const userTextMsg = document.getElementById('text-area');
 
-if (!localStorage.getItem('name')) {
-  populateForm();
-} else setLocal();
-
-function populateForm() {
-  localStorage.setItem('name', document.getElementById('name').value);
-  localStorage.setItem('email-id', document.getElementById('email-id').value);
-  localStorage.setItem('text-area', document.getElementById('text-area').value);
-  setLocal();
-}
-
 function setLocal() {
   const localName = localStorage.getItem('name');
   const localemail = localStorage.getItem('email-id');
@@ -22,7 +11,17 @@ function setLocal() {
   document.getElementById('text-area').value = localText;
 }
 
+function populateForm() {
+  localStorage.setItem('name', document.getElementById('name').value);
+  localStorage.setItem('email-id', document.getElementById('email-id').value);
+  localStorage.setItem('text-area', document.getElementById('text-area').value);
+  setLocal();
+}
+
+if (!localStorage.getItem('name')) {
+  populateForm();
+} else setLocal();
+
 userFullName.onchange = populateForm;
 userEmailId.onchange = populateForm;
 userTextMsg.onchange = populateForm;
-console.log(localStorage);
